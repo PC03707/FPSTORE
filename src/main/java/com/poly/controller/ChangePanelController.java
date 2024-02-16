eb324ec2809b4fe3f08714cd86cac59c476841b0
PC03707 <134180302+PC03707@users.noreply.github.com>
2024-02-16 15:42:17 +0700

    upload

---
+++ src/main/java/com/poly/controller/ChangePanelController.java
@@ -0,0 +1,22 @@
+package com.poly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.poly.dao.ChangePanelDAO;
+import com.poly.entity.ChangePanel;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/changepanels")
+public class ChangePanelController {
+
+
+//    @GetMapping("/{id}")
+//    public ChangePanel getChangePanelById(@PathVariable int id) {
+//        return changePanelService.getChangePanelById(id);
+//    }
+
+    // Các phương thức khác xử lý request
+}
---
+++ src/main/java/com/poly/dao/ImageDAO.java
@@ -0,0 +1,15 @@
+package com.poly.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poly.entity.Account;
+import com.poly.entity.Author;
+import com.poly.entity.Book;
+import com.poly.entity.Image;
+import java.util.List;
+
+
+public interface ImageDAO extends JpaRepository<Image, Integer>{
+		List<Image> findByBook(Book book);
+
+}
